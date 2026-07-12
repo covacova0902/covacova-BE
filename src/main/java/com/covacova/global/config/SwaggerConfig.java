ee3ec77288acedf65a31620882b9f9f36f637630
yellowmodric <kyr7091@naver.com>
2026-07-12 21:23:16 +0900

    feat: 스웨거 에 Authorization 추가

---
+++ src/main/java/com/covacova/global/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.covacova.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI().components(new Components().addSecuritySchemes(SCHEME_NAME, bearerScheme))
+                .addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME));
+    }
+}
